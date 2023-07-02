7fa7559b4dd57c8e526e27fc96aec8119ccdae87
Kira <kirastr354@gmail.com>
2023-07-02 17:49:31 +0300

    describe bot info in welcome message and more friendly error message

--- main.py
+++ main.py
@@ -14,14 +14,14 @@ bot = telebot.TeleBot('5897120145:AAEgh8bZr72solVQt3p405rUQod9Bz1-HWs')
 
 WELCOME_MESSAGE = u'Welcome to the Gift Assistant Bot! 🎁\nThis bot will help you to choose a present for your ' \
                   u'relatives and friends. Based on person characteristics, such as age, gender, interests, ' \
-                  u'and occasion information it will select the most appropriate gift options.\n' \ 
+                  u'and occasion information it will select the most appropriate gift options.\n'\
                   u'✔ To start, use command - /gift\n' \
                   u'✖ To interrupt, use the command - /cancel'
 
 
-MISTAKE_MESSAGE = u'error 404, start again\n'\
-                  u'To start use command - /gift\n' \
-                  u'To abort use command - /cancel'
+MISTAKE_MESSAGE = u'Information is not in correct format or caused system problem.\nPlease, start again\n'\
+                  u'✔ To start, use command - /gift\n' \
+                  u'✖ To interrupt, use the command - /cancel'
 
 
 # Start command handler
